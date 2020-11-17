23cf22fe2dc4194b00296b8d258276fe4f2f6636
kakigakki <kakigakki@yahoo.co.jp>
2020-11-18 00:16:42 +0900

    1

--- 数组/15.三数之和.js
+++ 数组/15.三数之和.js
@@ -10,30 +10,26 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    let res = []
-    if (nums == null || nums.length < 3) return [];
-    nums.sort((a, b) => a - b); // 排序
+    let left, right, res = []
+    nums.sort((a, b) => a - b)
     for (let i = 0; i < nums.length - 2; i++) {
-        if (nums[i] === nums[i - 1]) continue
-            //此时三数之和肯定大于0了 ，所以直接返回
+        if (nums[i - 1] === nums[i]) continue
         if (nums[i] > 0) return res
-        let left = i + 1
-        let right = nums.length - 1
+        left = i + 1
+        right = nums.length - 1
         while (left < right) {
-            const sum = nums[i] + nums[left] + nums[right]
+            const sum = nums[left] + nums[right] + nums[i]
             if (sum > 0) {
-                right--
+                while (nums[right] === nums[--right]) continue
             } else if (sum < 0) {
-                left++
+                while (nums[left] === nums[++left]) continue
             } else {
                 res.push([nums[i], nums[left], nums[right]])
-                    //去重
-                while (nums[left] == nums[++left]) continue
-                while (nums[right] == nums[--right]) continue
+                while (nums[right] === nums[--right]) continue
+                while (nums[left] === nums[++left]) continue
             }
         }
     }
-
     return res
 };
 // @lc code=end
